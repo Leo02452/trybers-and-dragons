597fac30d671f4b8f474392b486322b44f66474b
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-09 16:49:36 -0300

    feat: criada classe Warrior

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,8 +1,10 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
+import Warrior from './Warrior';
 
 export default Archetype;
 export { Mage,
   Necromancer,
+  Warrior,
 };
