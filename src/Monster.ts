2416c46e5140d44dd3ad4ad8cf5e6323cc0bf655
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-10 20:27:36 -0300

    feat: criada classe Monster
    Requisito 09 concluído

---
+++ src/Monster.ts
@@ -0,0 +1,34 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+  
+  receiveDamage(attackPoints: number): number {
+    const damage = this.lifePoints - attackPoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    } 
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+}
