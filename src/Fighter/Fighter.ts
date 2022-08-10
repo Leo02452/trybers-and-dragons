110eaff94ca3d7b62680430a1185ea9810d2374a
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-10 17:05:13 -0300

    refactor: interface Fighter modificada
    Agora a interface extende de SimpleFighter,
    herdando seus atributos e métodos

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,13 +1,10 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimplesFighter';
 
-export default interface Fighter {
-  lifePoints: number,
-  strength: number,
+export default interface Fighter extends SimpleFighter {
   defense: number,
   energy?: Energy,
 
-  attack(enemy: Fighter): void,
   special?(enemy: Fighter): void,
   levelUp(): void,
-  receiveDamage(attackPoints: number): number,
 }
