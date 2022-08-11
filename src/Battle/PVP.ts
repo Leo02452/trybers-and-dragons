45f9c48a648cbfabc271886a3676043acc0c82bd
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-10 22:23:04 -0300

    feat: criada classe PVP
    Requisito 10 concluído

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  player1: Fighter;
+  player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints !== -1 && this.player2.lifePoints !== -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    return super.fight();
+  }
+}
